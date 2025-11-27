819f78e32347188daf1f41a41e64869d2cb154bc
slimani <amineslimani1610@gmail.com>
2025-11-27 21:27:33 +0100

    Remove CreateArtisan route from App component

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,6 @@ import CreateTicket from './pages/CreateTicket';
 import { AuthProvider } from './contexts/AuthContext';
 import AdminDashboard from './pages/AdminDashboard';
 import ArtisanHome from './pages/ArtisanHome';
-import CreateArtisan from './pages/CreateArtisan';
 function App() {
   return (
     <AuthProvider> {/* 1. On enveloppe l'app avec la sécurité */}
@@ -23,7 +22,6 @@ function App() {
           <Route path="/create-ticket" element={<CreateTicket />} />
           <Route path="/app/admin" element={<AdminDashboard />} />
           <Route path="/app/artisan" element={<ArtisanHome />} />
-          <Route path="/admin/create-artisan" element={<CreateArtisan />} />
         </Routes>
       </BrowserRouter>
     </AuthProvider>
